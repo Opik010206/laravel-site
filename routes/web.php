420643513100570169bae15ba88195b824e48467
Ainur Rofiq <Opik010206@gmail.com>
2022-12-17 15:36:06 +0700

    percobaan pertama

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
+Route::get('/test', [HomeController::class,'index']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
